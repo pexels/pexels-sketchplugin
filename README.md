6ffcddaae4359d55a05e10caa3a4b774ef20b5dc
Ale M <bomberstudios@gmail.com>
2019-09-06 11:25:42 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,26 @@
+![Pexels Banner](docs/social-preview.png)
+
+# Pexels Plugin
+
+## Installation
+
+- [Download the latest version](https://github.com/BohemianCoding/pexels-sketchplugin/releases/latest/download/pexels.sketchplugin.zip).
+- Unzip & double click it to install.
+
+## Features
+
+Get images from [Pexels](https://pexels.com), using Sketch’s Data Supplier feature.
+
+You can use it from the toolbar Data icon…
+
+![Using the Pexels Plugin from the toolbar icon](docs/pexels-screenshot-001.png)
+
+…or from the contextual menu for any layer…
+
+![Using the Pexels Plugin from the contextual menu](docs/pexels-screenshot-002.png)
+
+…or even for Overrides using the Inspector:
+
+![Using the Pexels Plugin for Overrides from the Inspector](docs/pexels-screenshot-003.png)
+
+When you’ve set an image fill for a layer, you can later on open the original URL for an image, in case you need to get the full credits, or download the original file, etc. To do that, click Plugins › Pexels › View Photo on Pexels. It does not work for overrides yet, but we’re working on it!
---
+++ src/DataProvider.js
@@ -0,0 +1,183 @@
+const os = require('os')
+const path = require('path')
+const util = require('util')
+const fs = require('@skpm/fs')
+const sketch = require('sketch')
+const { getImagesURLsForItems } = require('./pexels')
+
+const { DataSupplier, UI, Settings } = sketch
+
+const SETTING_KEY = 'pexels.photo.id'
+const FOLDER = path.join(os.tmpdir(), 'com.sketchapp.pexels-plugin')
+
+export function onStartup () {
+  DataSupplier.registerDataSupplier('public.image', 'Random Photo', 'SupplyRandomPhoto')
+  DataSupplier.registerDataSupplier('public.image', 'Search Photo…', 'SearchPhoto')
+}
+
+export function onShutdown () {
+  DataSupplier.deregisterDataSuppliers()
+  try {
+    if (fs.existsSync(FOLDER)) {
+      fs.rmdirSync(FOLDER)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export function onSupplyRandomPhoto (context) {
+  setImageForContext(context)
+}
+
+function containsPhotoId (searchTerm) {
+  return searchTerm.substr(0, 3) === 'id:' || searchTerm.indexOf('pexels.com/photo/') !== -1
+}
+
+function extractPhotoId (searchTerm) {
+  if (searchTerm.substr(0, 3) === 'id:') {
+    return searchTerm.substr(3)
+  }
+
+  // Extract photoId from a "pexels.com/photo/image-title-<photoId>" URL
+  // Allows a URL with or without http/https
+  // It also strips out anything after the photoId
+  let photoId = searchTerm.substr(searchTerm.indexOf('unsplash.com/photos/') + 20)
+  const artifactLocation = photoId.search(/[^a-z0-9_-]/i)
+  return artifactLocation !== -1 ? photoId.substr(0, artifactLocation) : photoId
+}
+
+export function onSearchPhoto (context) {
+  // 21123: retrieve previous search term. If multiple layers are selected, find the first search term
+  // in the group…
+  let selectedLayers = sketch.getSelectedDocument().selectedLayers.layers
+  let previousTerms = selectedLayers.map(layer => Settings.layerSettingForKey(layer, 'pexels.search.term'))
+  let firstPreviousTerm = previousTerms.find(term => term !== undefined)
+  let previousTerm = firstPreviousTerm || 'People'
+  // TODO: support multiple selected layers with different search terms for each
+  if (sketch.version.sketch < 53) {
+    const searchTerm = UI.getStringFromUser('Search Pexels for…', previousTerm).trim()
+    if (searchTerm !== 'null') {
+      selectedLayers.forEach(layer => {
+        Settings.setLayerSettingForKey(layer, 'pexels.search.term', searchTerm)
+      })
+      if (containsPhotoId(searchTerm)) {
+        setImageForContext(context, null, extractPhotoId(searchTerm))
+      } else {
+        setImageForContext(context, searchTerm.replace(/\s+/g, '-').toLowerCase())
+      }
+    }
+  } else {
+    UI.getInputFromUser('Search Pexels for…',
+      { initialValue: previousTerm },
+      (err, searchTerm) => {
+        if (err) { return } // user hit cancel
+        if ((searchTerm = searchTerm.trim()) !== 'null') {
+          selectedLayers.forEach(layer => {
+            Settings.setLayerSettingForKey(layer, 'pexels.search.term', searchTerm)
+          })
+          if (containsPhotoId(searchTerm)) {
+            setImageForContext(context, null, extractPhotoId(searchTerm))
+          } else {
+            setImageForContext(context, searchTerm.replace(/\s+/g, '-').toLowerCase())
+          }
+        }
+      }
+    )
+  }
+}
+
+export default function onImageDetails () {
+  const selectedDocument = sketch.getSelectedDocument()
+  const selection = selectedDocument ? selectedDocument.selectedLayers : []
+  if (selection.length > 0) {
+    selection.forEach(element => {
+      const id = Settings.layerSettingForKey(element, SETTING_KEY) || (
+        element.type === 'SymbolInstance' &&
+        element.overrides
+          .map(o => Settings.layerSettingForKey(o, SETTING_KEY))
+          .find(s => !!s)
+      )
+      if (id) {
+        NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString(`https://pexels.com/photo/${id}`))
+      } else {
+        // This layer doesn't have an Pexels photo set, do nothing.
+        // Alternatively, show an explanation of what the user needs to do to make this work…
+        UI.message(`To get a random photo, click Data › Pexels › Random Photo in the toolbar, or right click the layer › Data Feeds › Pexels › Random Photo`)
+      }
+    })
+  } else {
+    UI.message(`Please select at least one layer`)
+  }
+}
+
+function setImageForContext (context, searchTerm, photoId) {
+  const dataKey = context.data.key
+  const items = util.toArray(context.data.items).map(sketch.fromNative)
+
+  UI.message('🕑 Downloading…')
+  getImagesURLsForItems(items, { searchTerm, photoId })
+    .then(res => Promise.all(res.map(({ data, item, index, frame, error }) => {
+      if (error) {
+        UI.message(error)
+        console.error(error)
+      } else {
+        process(data, dataKey, index, item, frame)
+      }
+    })))
+    .catch(e => {
+      UI.message(e)
+      console.error(e)
+    })
+}
+
+function process (data, dataKey, index, item, frame) {
+  // supply the data
+  // TODO: use the URL that's good for the frame, choosing from
+  // original, large2x, large, medium, small, portrait, landscape, tiny
+  return getImageFromURL(data.src.large, frame).then(imagePath => {
+    if (!imagePath) {
+      // TODO: something wrong happened, show something to the user
+      return
+    }
+    DataSupplier.supplyDataAtIndex(dataKey, imagePath, index)
+
+    // store where the image comes from, but only if this is a regular layer
+    if (item.type !== 'DataOverride') {
+      Settings.setLayerSettingForKey(item, SETTING_KEY, data.id)
+    }
+
+    UI.message('📷 by ' + data.photographer + ' on Pexels')
+  })
+}
+
+function getImageFromURL (url, frame) {
+  console.log(`getImageFromURL(${url})`)
+  return fetch(url)
+  // return fetch(`${url}&fit=min&w=${frame.width*4}&h=${frame.height*4}`)
+    .then(res => res.blob())
+    // TODO: use imageData directly, once #19391 is implemented
+    .then(saveTempFileFromImageData)
+    .catch((err) => {
+      console.error(err)
+      return context.plugin.urlForResourceNamed('placeholder.png').path()
+    })
+}
+
+function saveTempFileFromImageData (imageData) {
+  const guid = NSProcessInfo.processInfo().globallyUniqueString()
+  const imagePath = path.join(FOLDER, `${guid}.jpg`)
+  try {
+    fs.mkdirSync(FOLDER)
+  } catch (err) {
+    // probably because the folder already exists
+    // TODO: check that it is really because it already exists
+  }
+  try {
+    fs.writeFileSync(imagePath, imageData, 'NSData')
+    return imagePath
+  } catch (err) {
+    console.error(err)
+    return undefined
+  }
+}
